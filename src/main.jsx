433f54f48425d14783b6ff0552e093f1fa616a53
gercek1 <olcanchano@gmail.com>
2024-09-25 09:37:37 -0500

    ✨ [config3]
    firestore config y rutas

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css';
 import AppRouter from './router/AppRouter.jsx';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <AppRouter />
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </Provider>
 )
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Home from '../pages/Home'
+import { Routes, Route } from 'react-router-dom'
+import PublicRouter from './PublicRouter'
+import PrivateRoutes from './PrivateRoutes'
+
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home/>} />
+        <Route element={<PublicRouter isAutenticate={false} />}>
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+        </Route>
+        <Route element={<PrivateRoutes isAutenticate={false}/>}>
+            <Route path='news' element={<News/>}>
+                <Route path=':newid' element={<NewDetalles/>} />
+            </Route>
+        </Route>
+    </Route>
+    </Routes>
   )
 }
 
