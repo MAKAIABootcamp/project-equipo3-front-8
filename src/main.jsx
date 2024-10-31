f071928fb32a80adb19a0e86c336deedfe9f37bd
gercek1 <olcanchano@gmail.com>
2024-10-31 09:00:23 -0500

    critical error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 import AppRouter from './router/AppRouter.jsx';
 import store from './redux/store.js';
-import './styles/index.css';
+import './Styles/index.css';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store} >
